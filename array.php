bf3d9bbf12284da48ed66ff06b7ac79c10d2fd10
frhamel <svpra222@gmail.com>
2024-03-19 11:07:31 +0700

    belajar git

---
+++ array.php
@@ -0,0 +1,11 @@
+<?php
+   // pembuatan array
+   $nama = ["Andri","Joko","Sukma","Rina","Sari"];
+ 
+   // pengaksesan array
+   echo $nama[0]; 
+   echo "<br />";
+   echo $nama[2]; 
+   echo "<br />";
+   echo $nama[4]; 
+?>
